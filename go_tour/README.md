7a474de251b07281633b2f8df77e145c2c57a972
S-higss <shonoji2001@gmail.com>
2024-08-25 15:37:00 +0900

    [Add]first section

---
+++ go_tour/README.md
@@ -0,0 +1,2 @@
+# About this directory
+This directory is made based on [A Tour of Go](https://go-tour-jp.appspot.com/welcome/1) site.
---
+++ go_tour/sandbox.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func First_section() {
+	fmt.Println("Welcome to the playground!")
+	fmt.Println("The time is", time.Now())
+}
